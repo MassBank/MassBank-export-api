49a033612e892ac0e1768002f32d2751400db876
Rene Meier <meier.rene@googlemail.com>
2024-09-04 17:49:42 +0200

    First skeleton working

--- src/main/java/de/ipb_halle/massbank3_export_service/api/VersionApiDelegateImpl.java
+++ src/main/java/de/ipb_halle/massbank3_export_service/api/VersionApiDelegateImpl.java
@@ -1,33 +1,22 @@
 package de.ipb_halle.massbank3_export_service.api;
 
-import jakarta.annotation.Generated;
-import jakarta.validation.constraints.*;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
-/**
- * A delegate to be called by the {@link VersionApiController}}.
- * Implement this interface with a {@link org.springframework.stereotype.Service} annotated class.
- */
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-09-02T14:32:39.719208510+02:00[Europe/Oslo]", comments = "Generator version: 7.8.0")
-public interface VersionApiDelegateImpl {
-
-    default Optional<NativeWebRequest> getRequest() {
-        return Optional.empty();
-    }
-
+@Service
+public class VersionApiDelegateImpl implements VersionApiDelegate {
     /**
      * GET /version : Get the version string of the implementation.
      *
      * @return version string (status code 200)
      * @see VersionApi#versionGet
      */
-    default ResponseEntity<String> versionGet() {
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
-
+    @Override
+    public ResponseEntity<String> versionGet() {
+        return new ResponseEntity<>("1.0.0", HttpStatus.OK);
     }
 
 }
