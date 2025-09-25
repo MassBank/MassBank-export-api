22dffb16d57f6b1325e0ef2ffc35e61764180d17
René Meier <meier.rene@googlemail.com>
2025-09-25 13:05:05 +0200

    Add "massbank" option to endpoint /convert and add endpoint /rawtext

--- src/main/java/massbank_export_api/api/ConvertApiDelegateImpl.java
+++ src/main/java/massbank_export_api/api/ConvertApiDelegateImpl.java
@@ -5,20 +5,28 @@ import massbank_export_api.model.Conversion;
 import massbank.export.RecordToNIST_MSP;
 import massbank.export.RecordToRIKEN_MSP;
 import massbank.Record;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import static massbank_export_api.api.DataReader.*;
 
 
+@Primary
 @Service
-public class ConvertApiDelegateImpl implements massbank_export_api.api.ConvertApiDelegate {
+public class ConvertApiDelegateImpl implements ConvertApiDelegate {
     /**
      * POST /convert : Create a conversion task.
      *
@@ -27,34 +35,81 @@ public class ConvertApiDelegateImpl implements massbank_export_api.api.ConvertAp
      * @see ConvertApi#convertPost
      */
     @Override
-    public ResponseEntity<org.springframework.core.io.Resource> convertPost(Conversion conversion) {
-        String responseBody = switch (conversion.getFormat().getValue()) {
-            case "nist_msp" -> conversion.getRecordList().stream()
-                .map(recordMap::get)
-                .filter(Objects::nonNull)
-                .map(RecordToNIST_MSP::convert)
-                .collect(Collectors.joining("\n"));
-            case "riken_msp" -> conversion.getRecordList().stream()
-                .map(recordMap::get)
-                .filter(Objects::nonNull)
-                .map(RecordToRIKEN_MSP::convert)
-                .collect(Collectors.joining("\n"));
-            default -> conversion.getRecordList().stream()
-                .map(recordMap::get)
-                .filter(Objects::nonNull)
-                .map(Record::toString)
-                .collect(Collectors.joining("\n"));
+    public ResponseEntity<Resource> convertPost(Conversion conversion) {
+        final ByteArrayResource resource;
+        final String filename;
+        final MediaType mediaType;
+
+        switch (Objects.requireNonNull(conversion.getFormat()).getValue()) {
+            case "nist_msp":
+                mediaType = MediaType.TEXT_PLAIN;
+                filename = "records.msp";
+                resource = new ByteArrayResource(
+                        conversion.getRecordList().stream()
+                                .map(recordMap::get)
+                                .filter(Objects::nonNull)
+                                .map(RecordToNIST_MSP::convert)
+                                .collect(Collectors.joining())
+                                .getBytes(StandardCharsets.UTF_8)
+                );
+                break;
+            case "riken_msp":
+                mediaType = MediaType.TEXT_PLAIN;
+                filename = "records.msp";
+                resource = new ByteArrayResource(
+                        conversion.getRecordList().stream()
+                                .map(recordMap::get)
+                                .filter(Objects::nonNull)
+                                .map(RecordToRIKEN_MSP::convert)
+                                .collect(Collectors.joining())
+                                .getBytes(StandardCharsets.UTF_8)
+                );
+                break;
+            case "massbank":
+                mediaType = MediaType.parseMediaType("application/zip");
+                filename = "records.zip";
+                try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                     ZipOutputStream zos = new ZipOutputStream(baos, StandardCharsets.UTF_8)) {
+                    conversion.getRecordList().stream()
+                            .map(recordMap::get)
+                            .filter(Objects::nonNull)
+                            .forEach(record -> {
+                                try {
+                                    ZipEntry entry = new ZipEntry(record.ACCESSION()+".txt");
+                                    zos.putNextEntry(entry);
+                                    zos.write(record.toString().getBytes(StandardCharsets.UTF_8));
+                                    zos.closeEntry();
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            });
+                    zos.finish();
+                    resource = new ByteArrayResource(baos.toByteArray());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            default:
+                mediaType = MediaType.TEXT_PLAIN;
+                filename = "records.txt";
+                resource = new ByteArrayResource(
+                        conversion.getRecordList().stream()
+                                .map(recordMap::get)
+                                .filter(Objects::nonNull)
+                                .map(Record::toString)
+                                .collect(Collectors.joining("\n"))
+                                .getBytes(StandardCharsets.UTF_8)
+                );
         };
 
-        ByteArrayResource resource = new ByteArrayResource(responseBody.getBytes(StandardCharsets.UTF_8));
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=converted_records.txt");
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename);
 
         return ResponseEntity.ok()
             .headers(headers)
             .contentLength(resource.contentLength())
-            .contentType(org.springframework.http.MediaType.APPLICATION_OCTET_STREAM)
+            .contentType(mediaType)
             .body(resource);
     }
 
--- src/test/java/massbank_export_api/ExportApiControllerTest.java
+++ src/test/java/massbank_export_api/ExportApiControllerTest.java
@@ -6,8 +6,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.hamcrest.Matchers.startsWith;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(classes = org.openapitools.OpenApiGeneratorApplication.class)
@@ -27,7 +35,7 @@ public class ExportApiControllerTest {
 
     @Test
     public void testCreateConversionTaskNistMSP() throws Exception {
-        String requestBody = "{ \"record_list\": [\"MSBNK-IPB_Halle-PB001341\"], \"format\": \"nist_msp\" }";
+        String requestBody = "{ \"record_list\": [\"MSBNK-IPB_Halle-PB001341\" , \"MSBNK-IPB_Halle-PB000125\"], \"format\": \"nist_msp\" }";
         String expectedResponse = """
 Name: Rutin
 Synon: 2-(3,4-dihydroxyphenyl)-5,7-dihydroxy-3-[(2S,3R,4S,5S,6R)-3,4,5-trihydroxy-6-[[(2R,3R,4R,5R,6S)-3,4,5-trihydroxy-6-methyloxan-2-yl]oxymethyl]oxan-2-yl]oxychromen-4-one
@@ -53,19 +61,67 @@ Num Peaks: 5
 465.102 587
 611.161 669
 
+Name: Naringenin
+Synon: 5,7-dihydroxy-2-(4-hydroxyphenyl)chroman-4-one
+DB#: MSBNK-IPB_Halle-PB000125
+InChIKey: FTVWIRXFELQLPI-ZDUSSCGKSA-N
+InChI: InChI=1S/C15H12O5/c16-9-3-1-8(2-4-9)13-7-12(19)15-11(18)5-10(17)6-14(15)20-13/h1-6,13,16-18H,7H2/t13-/m0/s1
+SMILES: C1[C@H](OC2=CC(=CC(=C2C1=O)O)O)C3=CC=C(C=C3)O
+Precursor_type: [M+H]+
+Spectrum_type: MS2
+Instrument_type: LC-ESI-QTOF
+Instrument: API QSTAR Pulsar i
+Ion_mode: POSITIVE
+Collision_energy: 55 eV
+Formula: C15H12O5
+MW: 272
+ExactMass: 272.06847
+Comments: Parent=-1
+Splash: splash10-0gbc-9800000000-79055ba218dabe14501d
+Num Peaks: 29
+53.037 10
+55.017 70
+65.038 111
+67.018 165
+68.997 238
+69.035 194
+77.039 59
+79.018 32
+81.034 12
+83.014 46
+85.030 8
+91.054 774
+92.058 15
+92.999 10
+95.050 91
+97.029 90
+107.049 63
+111.011 37
+115.054 28
+119.050 538
+120.054 13
+123.044 23
+125.022 17
+128.060 19
+147.043 58
+152.060 30
+153.018 999
+154.024 43
+157.068 9
+
 """;
 
         mockMvc.perform(post("/convert")
                 .contentType("application/json")
                 .content(requestBody))
             .andExpect(status().isOk())
-            .andExpect(content().contentType("application/octet-stream"))
+            .andExpect(content().contentType("text/plain"))
             .andExpect(content().string(expectedResponse));
     }
 
     @Test
     public void testCreateConversionTaskRikenMSP() throws Exception {
-        String requestBody = "{ \"record_list\": [\"MSBNK-IPB_Halle-PB001341\"], \"format\": \"riken_msp\" }";
+        String requestBody = "{ \"record_list\": [\"MSBNK-IPB_Halle-PB001341\" , \"MSBNK-IPB_Halle-PB000125\"], \"format\": \"riken_msp\" }";
         String expectedResponse = """
 NAME: Rutin
 PRECURSORMZ:\s
@@ -89,14 +145,97 @@ Num Peaks: 5
 465.102	5884.210
 611.161	6700.000
 
+NAME: Naringenin
+PRECURSORMZ:\s
+PRECURSORTYPE: [M+H]+
+FORMULA: C15H12O5
+Ontology: Flavans
+INCHIKEY: FTVWIRXFELQLPI-ZDUSSCGKSA-N
+INCHI: InChI=1S/C15H12O5/c16-9-3-1-8(2-4-9)13-7-12(19)15-11(18)5-10(17)6-14(15)20-13/h1-6,13,16-18H,7H2/t13-/m0/s1
+SMILES: C1[C@H](OC2=CC(=CC(=C2C1=O)O)O)C3=CC=C(C=C3)O
+RETENTIONTIME: 0
+INSTRUMENTTYPE: LC-ESI-QTOF
+INSTRUMENT: API QSTAR Pulsar i
+IONMODE: Positive
+LINKS: INCHIKEY:FTVWIRXFELQLPI-ZDUSSCGKSA-N; KEGG:C00509; PUBCHEM:CID:439246; COMPTOX:DTXSID1022392
+Comment: DB#=MSBNK-IPB_Halle-PB000125; origin=MassBank; IPB_RECORD: 83; Annotation confident structure
+Splash: splash10-0gbc-9800000000-79055ba218dabe14501d
+Num Peaks: 29
+53.037	111.391
+55.017	718.038
+65.038	1125.168
+67.018	1665.237
+68.997	2395.872
+69.035	1956.044
+77.039	605.468
+79.018	331.546
+81.034	135.272
+83.014	471.723
+85.030	98.767
+91.054	7753.953
+92.058	168.025
+92.999	114.286
+95.050	921.201
+97.029	914.232
+107.049	644.063
+111.011	382.203
+115.054	299.384
+119.050	5392.656
+120.054	146.851
+123.044	249.397
+125.022	186.626
+128.060	202.332
+147.043	597.159
+152.060	313.053
+153.018	10000.000
+154.024	440.901
+157.068	101.153
+
 """;
 
         mockMvc.perform(post("/convert")
                 .contentType("application/json")
                 .content(requestBody))
             .andExpect(status().isOk())
-            .andExpect(content().contentType("application/octet-stream"))
+            .andExpect(content().contentType("text/plain"))
             .andExpect(content().string(expectedResponse));
+    }
+
+    @Test
+    public void testCreateConversionTaskMassBank() throws Exception {
+        String requestBody = "{ \"record_list\": [\"MSBNK-IPB_Halle-PB001341\" , \"MSBNK-IPB_Halle-PB000125\"], \"format\": \"massbank\" }";
+
+        byte[] zipBytes = mockMvc.perform(post("/convert")
+                        .contentType("application/json")
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/zip"))
+                .andReturn()
+                .getResponse()
+                .getContentAsByteArray();
+
+        List<String> fileNames = new ArrayList<>();
+        Map<String, byte[]> extractedFiles = new HashMap<>();
+        try (java.util.zip.ZipInputStream zis = new java.util.zip.ZipInputStream(new java.io.ByteArrayInputStream(zipBytes))) {
+            java.util.zip.ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                fileNames.add(entry.getName());
+                extractedFiles.put(entry.getName(), zis.readAllBytes());
+            }
+        }
+
+        assertTrue(fileNames.contains("MSBNK-IPB_Halle-PB001341.txt"));
+        assertTrue(fileNames.contains("MSBNK-IPB_Halle-PB000125.txt"));
+        assertEquals(2, fileNames.size());
+
+        for (String fileName : fileNames) {
+            try (InputStream is = getClass().getResourceAsStream("/MassBank-data-test/IPB_Halle/" + fileName)) {
+                assertNotNull(is, "Resource " + fileName + " not found in test resources");
+                byte[] expected = is.readAllBytes();
+                byte[] actual = extractedFiles.get(fileName);
+                assertArrayEquals(expected, actual, "File content mismatch for " + fileName);
+            }
+        }
     }
 
     @Test
@@ -202,6 +341,53 @@ Num Peaks: 5
             .andExpect(status().isOk())
             .andExpect(content().contentType("application/json"))
             .andExpect(content().json(expectedResponse));
+    }
+
+    @Test
+    public void testGetRawtext() throws Exception {
+        String accession = "MSBNK-IPB_Halle-PB001341";
+        String expectedResponse = """
+ACCESSION: MSBNK-IPB_Halle-PB001341
+RECORD_TITLE: Rutin; LC-ESI-QTOF; MS2; CE:10 eV; [M+H]+
+DATE: 2016.01.19 (Created 2008.05.22, modified 2013.06.04)
+AUTHORS: Boettcher C, Institute of Plant Biochemistry, Halle, Germany
+LICENSE: CC BY-SA
+COMMENT: IPB_RECORD: 541
+COMMENT: CONFIDENCE confident structure
+CH$NAME: Rutin
+CH$NAME: 2-(3,4-dihydroxyphenyl)-5,7-dihydroxy-3-[(2S,3R,4S,5S,6R)-3,4,5-trihydroxy-6-[[(2R,3R,4R,5R,6S)-3,4,5-trihydroxy-6-methyloxan-2-yl]oxymethyl]oxan-2-yl]oxychromen-4-one
+CH$COMPOUND_CLASS: Natural Product; Flavonol
+CH$FORMULA: C27H30O16
+CH$EXACT_MASS: 610.15338
+CH$SMILES: C[C@H]1[C@@H]([C@H]([C@H]([C@@H](O1)OC[C@@H]2[C@H]([C@@H]([C@H]([C@@H](O2)OC3=C(OC4=CC(=CC(=C4C3=O)O)O)C5=CC(=C(C=C5)O)O)O)O)O)O)O)O
+CH$IUPAC: InChI=1S/C27H30O16/c1-8-17(32)20(35)22(37)26(40-8)39-7-15-18(33)21(36)23(38)27(42-15)43-25-19(34)16-13(31)5-10(28)6-14(16)41-24(25)9-2-3-11(29)12(30)4-9/h2-6,8,15,17-18,20-23,26-33,35-38H,7H2,1H3/t8-,15+,17-,18+,20+,21-,22+,23+,26+,27-/m0/s1
+CH$LINK: INCHIKEY IKGXIBQEEMLURG-NVPNHPEKSA-N
+CH$LINK: KEGG C05625
+CH$LINK: PUBCHEM CID:5280805
+CH$LINK: COMPTOX DTXSID3022326
+CH$LINK: ChemOnt CHEMONTID:0001111; Organic compounds; Phenylpropanoids and polyketides; Flavonoids; Flavonoid glycosides
+AC$INSTRUMENT: API QSTAR Pulsar i
+AC$INSTRUMENT_TYPE: LC-ESI-QTOF
+AC$MASS_SPECTROMETRY: MS_TYPE MS2
+AC$MASS_SPECTROMETRY: ION_MODE POSITIVE
+AC$MASS_SPECTROMETRY: COLLISION_ENERGY 10 eV
+AC$MASS_SPECTROMETRY: IONIZATION ESI
+MS$FOCUSED_ION: PRECURSOR_TYPE [M+H]+
+PK$SPLASH: splash10-0wmi-0009506000-98ca7f7c8f3072af4481
+PK$NUM_PEAK: 5
+PK$PEAK: m/z int. rel.int.
+  147.063 121.684 11
+  303.050 10000.000 999
+  449.108 657.368 64
+  465.102 5884.210 587
+  611.161 6700.000 669
+//
+""";
+
+        mockMvc.perform(get("/rawtext/{accession}", accession))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain"))
+                .andExpect(content().string(expectedResponse));
     }
 
 }
